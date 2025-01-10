47a0c546cb3a0bed3bdf662c2f3114a8e29bfee2
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-01-10 19:15:37 +0800

    fixed pre_commit for sub categories

--- pys/pre_commit.py
+++ pys/pre_commit.py
@@ -41,14 +41,13 @@ ignore = False
 subcat_list = []
 incomplete_pkics = {}
 packs = -1
-pack_list = []
 name_to_json = {}
 priority = {}
 
-with open(f"{cdir()}/jsons/others/pack_order_list.txt","r") as pol:
-    for i in pol:
-        pack_list.append(i)
 
+with open(f"{cdir()}/jsons/others/pack_order_list.txt","r") as pol:
+    cat_list = pol.read().split("\n")
+print(cat_list)
 parser = argparse.ArgumentParser(description='Run a massive script that updates Packs to-do, Icons to-do, Compatibilities to-do and the HTML')
 parser.add_argument('--format', '-f', action='store_true', help='Include flag to format files')
 parser.add_argument('--only-update-html', '-ouh', action='store_true', help='Only update the HTML')
@@ -58,6 +57,8 @@ parser.add_argument('--update-theme', '-ut', action='store_true', help='Pulls th
 parser.add_argument('--log-error', '-el', action='store_true', help='Prints out errors')
 parser.add_argument('--no-stash', '-ns', action='store_true', help='Does not stash changes')
 args = parser.parse_args()
+if args.build == None:
+    args.build = ""
 
 if not args.no_stash:
     clrprint("Stashing changes...", clr="yellow")
@@ -69,11 +70,9 @@ if not args.no_stash:
 if not "site" in args.build or (args.build and (args.only_update_html or args.only_update_jsons or args.format)):
     clrprint("Going through Packs...", clr="yellow")
     id_to_name = {}
-    for j in pack_list:
+    for j in cat_list:
         origj = j
         if not ignore:
-            if j.endswith("\n"):
-                j = j[:-1]
             file = load_json(f"{cdir()}/jsons/packs/{j}")
             name_to_json[file["topic"]] = j
             # Adds the categories automatically
@@ -205,33 +204,33 @@ if not "site" in args.build or (args.build and (args.only_update_html or args.on
                         dump_json(f"{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/list.json", listjson)
                     id_to_name[file["packs"][i]["pack_id"]] = file["packs"][i]["pack_name"]
         html = html.replace("<all_packs>", LZString.compressToEncodedURIComponent(dumps(current_category_packs)))
+        # handle subcategories
         try:
-            if pack_list[pack_list.index(origj) + 1].startswith("\t"):
+            indexoforigj = cat_list.index(origj)
+            if "\t" in cat_list[indexoforigj + 1] or "    " in cat_list[indexoforigj + 1]:
                 html += cat_end_w_subcat_no_end
                 try:
-                    if not pack_list[pack_list.index(origj) + 2].startswith("\t"):
+                    if "\t" in cat_list[indexoforigj + 1] or "    " in cat_list[indexoforigj + 1]:
                         html += category_end
                 except IndexError:
                     pass
                 html = html.replace("<index>", str(subcats))
                 subcats += 1
                 ignore = True
-                subcat_list.append(pack_list[pack_list.index(origj) + 1][1:])
+                if "\t" in cat_list[indexoforigj + 1]:
+                    subcat_list.append(cat_list[indexoforigj + 1][1:])
+                elif "    " in cat_list[indexoforigj + 1]:
+                    subcat_list.append(cat_list[indexoforigj + 1][4:])
             elif not ignore:
                 html += category_end
             else:
                 ignore = False
         except IndexError:
             html += category_end
-    """ Subcategories don't exist yet:tm:
     # Seperate loop for subcategories (I'm inefficient)
     for j in range(len(subcat_list)):
         pack_html = ""
         k = subcat_list[j]
-        if k.endswith("\n"):
-            k = k[:-1]
-        if k.startswith("\t"):
-            k = k[1:]
         file = load_json(f"{cdir()}/jsons/packs/{k}")
         name_to_json[file["topic"]] = k
         # Adds the categories automatically
@@ -239,20 +238,32 @@ if not "site" in args.build or (args.build and (args.only_update_html or args.on
         incomplete_packs[file["topic"]] = []
         pack_html += subcategory_start.replace("topic_name", f'{file["subcategory_of"]} > <b>{file["topic"]}</b>')
         current_category_packs = { "raw": [] }
+        # Runs through the packs
         for i in range(len(file["packs"])):
-            # Updates Incomplete Packs
+            # Build if neccessary
+            if "pack" in args.build:
+                try:
+                    clrprint("Building", file["packs"][i]["pack_id"], "with", file["packs"][i]["build"]["with"], clr="y,b,y,b")
+                    os.chdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/')
+                    os.system(f'{file["packs"][i]["build"]["with"]} {file["packs"][i]["build"]["script"]}')
+                except KeyError:
+                    pass # no need to build
+                # Updates Incomplete Packs
             try:
                 if os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/files') == []:
                     # Adds the packid to the topic list
                     incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
                     stats[1] += 1
+                    if args.log_error:
+                        clrprint("[packs]", "Incomplete Pack:", file["packs"][i]["pack_id"], clr="r,w,y")
                 else:
                     # When the packid directory has stuff inside
                     stats[0] += 1
             except FileNotFoundError:
-                # If the packs have not updated with the new directory type
+                # If the packs don't even exist
                 stats[1] += 1
-                incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
+                if args.log_error:
+                    clrprint("[packs]", "Incomplete Pack:", file["packs"][i]["pack_id"], clr="r,w,y")
 
             # Updates Incomplete pack_icon.png
             try:
@@ -273,11 +284,15 @@ if not "site" in args.build or (args.build and (args.only_update_html or args.on
                         # When pack icon doesn't even exist
                         incomplete_pkics[file["topic"]].append(file["packs"][i]["pack_id"])
                         pkicstats[1] += 1
+                        if args.log_error:
+                            clrprint("[packs]", "[icon]", "Incomplete Pack:", file["packs"][i]["pack_id"], clr="r,b,w,y")
                 except KeyError:
-                    incomplete_pkics[file["topic"]].append(file["packs"][i]["pack_id"])
-                    pkicstats[1] += 1
-            
-            # Updates Pack Conflicts
+                        incomplete_pkics[file["topic"]].append(file["packs"][i]["pack_id"])
+                        pkicstats[1] += 1
+                        if args.log_error:
+                            clrprint("[packs]", "[icon]", "Incomplete Pack:", file["packs"][i]["pack_id"], clr="r,b,w,y")
+
+            # Adds Pack Conflicts
             conflicts[file["packs"][i]["pack_id"]] = []
             try:
                 for conf in range(len(file["packs"][i]["conflict"])):
@@ -286,7 +301,13 @@ if not "site" in args.build or (args.build and (args.only_update_html or args.on
                 pass # If it is empty, it just skips
             if conflicts[file["packs"][i]["pack_id"]] == []:
                 del conflicts[file["packs"][i]["pack_id"]]
-            
+
+            # Add priority map
+            try:
+                priority[file["packs"][i]["pack_id"]] = file["packs"][i]["priority"]
+            except KeyError:
+                priority[file["packs"][i]["pack_id"]] = 0
+
             # Adds respective HTML
             confs = ""
             if file["packs"][i]["pack_id"] not in incomplete_packs[file["topic"]]:
@@ -321,17 +342,29 @@ if not "site" in args.build or (args.build and (args.only_update_html or args.on
                 to_add_pack = to_add_pack.replace("relloctopackicon", f'packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png')
                 try:
                     if os.path.exists(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.{file["packs"][i]["icon"]}'):
-                        # Because I can't make the html use a missing texture thing, some
+                        # Because I can't make the html use a missing texture thing, so
                         # it only replaces when it exists
                         to_add_pack = to_add_pack.replace("png", file["packs"][i]["icon"])
                 except KeyError:
                     pass
                 pack_html += to_add_pack
                 current_category_packs["raw"].append(file["packs"][i]["pack_id"])
+                if not args.only_update_html:
+                    listjson = []
+                    for root, _, files in os.walk(f"{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}"):
+                        for lsfile in files:
+                            filepath = os.path.relpath(os.path.join(root, lsfile),f"{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}")
+                            if len(filepath.split(os.path.sep)) == 1:
+                                continue
+                            else:
+                                listjson.append("/".join(filepath.split(os.path.sep)[1:]))
+                            listjson.sort()
+                    dump_json(f"{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/list.json", listjson)
+                id_to_name[file["packs"][i]["pack_id"]] = file["packs"][i]["pack_name"]
         pack_html += category_end
         html = html.replace(f'<div class="subcat{j}"></div>',pack_html)
         html = html.replace("<all_packs>", LZString.compressToEncodedURIComponent(dumps(current_category_packs)))
-    """
+    # compatibilities
     compatibilities = load_json(f"{cdir()}/jsons/packs/compatibilities.json")
     for ways in range(compatibilities["maxway"],1,-1):
         for location in compatibilities[f"{ways}way"]["locations"]:
